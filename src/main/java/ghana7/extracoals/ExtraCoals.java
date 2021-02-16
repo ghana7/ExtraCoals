fd7e8d3825ecebec62567ed13deff467a4a2fbd5
Jimmie Harkins <ghana796@gmail.com>
2021-02-15 22:45:15 -0500

    Torch update - added five cosmetic torches crafted with the custom coals

--- src/main/java/ghana7/extracoals/ExtraCoals.java
+++ src/main/java/ghana7/extracoals/ExtraCoals.java
@@ -1,13 +1,19 @@
 package ghana7.extracoals;
 
+import ghana7.extracoals.torches.*;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.WallOrFloorItem;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -26,6 +32,7 @@ public class ExtraCoals
     public static final String MODID = "extracoals";
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
     public static final RegistryObject<Item> DIAMOND_COAL = ITEMS.register("diamond_coal", () ->
             new Item(new Item.Properties()
@@ -61,10 +68,88 @@ public class ExtraCoals
                     .group(ItemGroup.MATERIALS)
                     .maxStackSize(64))
     );
+
+    public static final RegistryObject<Block> DIAMOND_TORCH = BLOCKS.register("diamond_torch", () ->
+            new DiamondTorch()
+    );
+
+    public static final RegistryObject<Block> EMERALD_TORCH = BLOCKS.register("emerald_torch", () ->
+            new EmeraldTorch()
+    );
+
+    public static final RegistryObject<Block> GOLD_TORCH = BLOCKS.register("gold_torch", () ->
+            new GoldTorch()
+    );
+
+    public static final RegistryObject<Block> LAPIS_TORCH = BLOCKS.register("lapis_torch", () ->
+            new LapisTorch()
+    );
+
+    public static final RegistryObject<Block> IRON_TORCH = BLOCKS.register("iron_torch", () ->
+            new IronTorch()
+    );
+
+    public static final RegistryObject<Block> DIAMOND_WALL_TORCH = BLOCKS.register("diamond_wall_torch", () ->
+            new DiamondWallTorch()
+    );
+
+    public static final RegistryObject<Block> EMERALD_WALL_TORCH = BLOCKS.register("emerald_wall_torch", () ->
+            new EmeraldWallTorch()
+    );
+
+    public static final RegistryObject<Block> GOLD_WALL_TORCH = BLOCKS.register("gold_wall_torch", () ->
+            new GoldWallTorch()
+    );
+
+    public static final RegistryObject<Block> LAPIS_WALL_TORCH = BLOCKS.register("lapis_wall_torch", () ->
+            new LapisWallTorch()
+    );
+
+    public static final RegistryObject<Block> IRON_WALL_TORCH = BLOCKS.register("iron_wall_torch", () ->
+            new IronWallTorch()
+    );
+
+    public static final RegistryObject<Item> DIAMOND_TORCH_BI = ITEMS.register("diamond_torch", () ->
+            new WallOrFloorItem(DIAMOND_TORCH.get(), DIAMOND_WALL_TORCH.get(), (new Item.Properties()).group(ItemGroup.DECORATIONS))
+    );
+
+    public static final RegistryObject<Item> EMERALD_TORCH_BI = ITEMS.register("emerald_torch", () ->
+            new WallOrFloorItem(EMERALD_TORCH.get(), EMERALD_WALL_TORCH.get(), (new Item.Properties()).group(ItemGroup.DECORATIONS))
+    );
+
+    public static final RegistryObject<Item> GOLD_TORCH_BI = ITEMS.register("gold_torch", () ->
+            new WallOrFloorItem(GOLD_TORCH.get(), GOLD_WALL_TORCH.get(), (new Item.Properties()).group(ItemGroup.DECORATIONS))
+    );
+
+    public static final RegistryObject<Item> LAPIS_TORCH_BI = ITEMS.register("lapis_torch", () ->
+            new WallOrFloorItem(LAPIS_TORCH.get(), LAPIS_WALL_TORCH.get(), (new Item.Properties()).group(ItemGroup.DECORATIONS))
+    );
+
+    public static final RegistryObject<Item> IRON_TORCH_BI = ITEMS.register("iron_torch", () ->
+            new WallOrFloorItem(IRON_TORCH.get(), IRON_WALL_TORCH.get(), (new Item.Properties()).group(ItemGroup.DECORATIONS))
+    );
+
+
     public ExtraCoals() {
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void onClientSetup(final FMLClientSetupEvent event) {
+        RenderTypeLookup.setRenderLayer(DIAMOND_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(EMERALD_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(GOLD_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(LAPIS_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(IRON_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(DIAMOND_WALL_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(EMERALD_WALL_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(GOLD_WALL_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(LAPIS_WALL_TORCH.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(IRON_WALL_TORCH.get(), RenderType.getCutout());
     }
     @Mod.EventBusSubscriber
     public static class FuelEvents{
---
+++ src/main/java/ghana7/extracoals/torches/EmeraldWallTorch.java
@@ -0,0 +1,32 @@
+package ghana7.extracoals.torches;
+
+import ghana7.extracoals.ExtraCoals;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.WallTorchBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.particles.IParticleData;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
+
+public class EmeraldWallTorch extends WallTorchBlock {
+    public EmeraldWallTorch() {
+        super(AbstractBlock.Properties.create(Material.MISCELLANEOUS).doesNotBlockMovement().zeroHardnessAndResistance().setLightLevel((state) -> {
+            return 15;
+        }).sound(SoundType.WOOD).lootFrom(ExtraCoals.EMERALD_TORCH.get()), ParticleTypes.FLAME);
+    }
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        double d0 = (double)pos.getX() + 0.5D;
+        double d1 = (double)pos.getY() + 0.7D;
+        double d2 = (double)pos.getZ() + 0.5D;
+        worldIn.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+    }
+}
